b15f17e77764472ed84db207316225b9a6b8ca2c
Blake Joynes <blakejoynes@gmail.com>
2018-01-28 16:44:18 -0500

    db setup script update

--- db_setup.py
+++ db_setup.py
@@ -1,12 +1,12 @@
 import pymysql
 import dbconfig
-connectionn= pymysql.connect(host='localhost',user=dbconfig.db_user,passwd=dbconfig.db_password)
+connectionn = pymysql.connect(host='localhost',user=dbconfig.db_user,passwd=dbconfig.db_password)
 
 try:
     with connection.cursor() as cursor:
-            sql = "CREATE DATABASE IF NOT EXISTS crimemap"
-            cursor.execute(sql)
-            sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (id  int NOT NULL AUTO_INCREMENT,
+    sql = "CREATE DATABASE IF NOT EXISTS crimemap"
+    cursor.execute(sql)
+    sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (id  int NOT NULL AUTO_INCREMENT,
             latitude FLOAT(10,6),
             longitude FLOAT(10,6),
             date DATETIME,
@@ -14,7 +14,7 @@ try:
             description VARCHAR(1000),
             updated_at TIMESTAMP,
             PRIMARY KEY (id))"""
-            cursor.execute(sql)
-            connection.commit()
+    cursor.execute(sql)
+    connection.commit()
 finally:
-        connection.close()
+    connection.close()
